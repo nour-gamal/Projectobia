e3fb4cb5cb8175be8d2ebe0a90313be09f2c0b27
Nour <nmg181963@hotmail.com>
2020-10-27 23:16:56 +0200

    Update ProjectDetails.js

--- src/Components/Projects/ProjectDetails.js
+++ src/Components/Projects/ProjectDetails.js
@@ -49,5 +49,5 @@ const mapstateToProps = (state, ownProps) => {
 
 export default compose(
 	connect(mapstateToProps),
-	firestoreConnect([{ collection: "Projects" }])
+	firestoreConnect([{ collection: "Projects" , orderBy: ["createdAt", "desc"] }])
 )(ProjectDetails);
